f803097a59474f6c4126707e54264e26e2f2ae09
BPDEV2000\Sizwe <sizwe@bbd.co.za>
2018-02-08 14:36:03 +0200

    upload to git

---
+++ taskCancellationToken/taskCancellationToken/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace taskCancellationToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task myTask = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+
+                token.ThrowIfCancellationRequested();
+
+            }, token);
+            try
+            {
+
+                Console.WriteLine("Press enter to stop the process:");
+                Console.ReadLine();
+                cancellationTokenSource.Cancel();
+                myTask.Wait();
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.InnerExceptions[0].Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("other expection: " + ex.Message);
+            }
+
+            Console.WriteLine("Press enter to end the program");
+            Console.ReadLine();
+        }
+    }
+}
